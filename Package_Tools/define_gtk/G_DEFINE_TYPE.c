4ab78059be48cac848b2776754e7d5c03a64d837
tux <you@example.com>
2024-09-03 15:29:20 +0200

    streamer

--- Package_Tools/define_gtk/G_DEFINE_TYPE.c
+++ Package_Tools/define_gtk/G_DEFINE_TYPE.c
@@ -53,40 +53,58 @@ static gpointer t_double_parent_class =
 
 static gint TDouble_private_offset; 
 static void t_double_class_intern_init (gpointer klass) 
-{ t_double_parent_class = g_type_class_peek_parent (klass); 
+{ 
+  t_double_parent_class = g_type_class_peek_parent (klass); 
   if (TDouble_private_offset != 0) g_type_class_adjust_private_offset (klass, &TDouble_private_offset); 
   t_double_class_init ((TDoubleClass*) klass); 
 } 
+
 static inline gpointer t_double_get_instance_private (TDouble *self) 
 { 
   return (((gpointer) ((guint8*) (self) + (glong) (TDouble_private_offset)))); 
 } 
+
 GType t_double_get_type (void) 
-{ static GType static_g_define_type_id = 0; 
-  if ((__extension__ ({ _Static_assert (sizeof *(&static_g_define_type_id) == sizeof (gpointer), "Expression evaluates to false"); 
-  (void) (0 ? (gpointer) * (&static_g_define_type_id) : ((void *)0)); 
-(!(__extension__ ({ _Static_assert (sizeof *(&static_g_define_type_id) == sizeof (gpointer), "Expression evaluates to false");
-   __typeof__ (*(&static_g_define_type_id)) gapg_temp_newval; 
-  __typeof__ ((&static_g_define_type_id)) gapg_temp_atomic = (&static_g_define_type_id); 
-  __atomic_load (gapg_temp_atomic, &gapg_temp_newval, 5); 
-  gapg_temp_newval; })) && g_once_init_enter_pointer (&static_g_define_type_id)); })) ) 
 { 
-  GType g_define_type_id = t_double_get_type_once (); 
-  (__extension__ ({ _Static_assert (sizeof *(&static_g_define_type_id) == sizeof (gpointer), "Expression evaluates to false"); 
-  0 ? (void) (*(&static_g_define_type_id) = (g_define_type_id)) : (void) 0; 
-  g_once_init_leave_pointer ((&static_g_define_type_id), (gpointer) (guintptr) (g_define_type_id)); 
-})) ; 
-} 
-return static_g_define_type_id;
- } __attribute__ ((__noinline__)) static GType t_double_get_type_once (void) 
+  static GType static_g_define_type_id = 0; 
+  if ((__extension__ (
+  { 
+    _Static_assert (sizeof *(&static_g_define_type_id) == sizeof (gpointer), "Expression evaluates to false"); 
+    (void) (0 ? (gpointer) * (&static_g_define_type_id) : ((void *)0)); 
+    (!(__extension__ (
+    { 
+      _Static_assert (sizeof *(&static_g_define_type_id) == sizeof (gpointer), "Expression evaluates to false");
+      __typeof__ (*(&static_g_define_type_id)) gapg_temp_newval; 
+      __typeof__ ((&static_g_define_type_id)) gapg_temp_atomic = (&static_g_define_type_id); 
+      __atomic_load (gapg_temp_atomic, &gapg_temp_newval, 5); 
+      gapg_temp_newval; 
+    }
+    )) && g_once_init_enter_pointer (&static_g_define_type_id)); 
+  }
+  )) ) 
+  { 
+    GType g_define_type_id = t_double_get_type_once (); 
+    (__extension__ (
+    { 
+      _Static_assert (sizeof *(&static_g_define_type_id) == sizeof (gpointer), "Expression evaluates to false"); 
+      0 ? (void) (*(&static_g_define_type_id) = (g_define_type_id)) : (void) 0; 
+      g_once_init_leave_pointer ((&static_g_define_type_id), (gpointer) (guintptr) (g_define_type_id)); 
+    }
+    )) ; 
+  } 
+  return static_g_define_type_id;
+ } 
+
+__attribute__ ((__noinline__)) static GType t_double_get_type_once (void) 
 { 
   GType g_define_type_id = g_type_register_static_simple (((GType) ((20) << (2))), g_intern_static_string ("TDouble"), sizeof (TDoubleClass), (GClassInitFunc)(void (*)(void)) t_double_class_intern_init, sizeof (TDouble), (GInstanceInitFunc)(void (*)(void)) t_double_init, (GTypeFlags) 0); 
-{ 
-  {
+  { 
     {
-    };
+      {
+      };
+    } 
   } 
-} return g_define_type_id; 
+ return g_define_type_id; 
 }
 
 
--- gstreamer/readme.md
+++
@@ -1,8 +0,0 @@
-## Examples
-
-https://github.com/nnstreamer/nnstreamer-example/blob/main/native/example_pose_estimation_tflite/nnstreamer_example_pose_estimation_tflite.cc
-
-
-## Alternative
-https://fluendo.com/fluendo-sdk/docs/api/fluendo-sdk-Player.html
-
